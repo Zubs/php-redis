9b42d6b62fe0b1f4e36fed5724ebf19490dd3d47
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2023-04-28 18:23:51 +0100

    feat: complete

--- public/index.php
+++ public/index.php
@@ -2,22 +2,53 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use GuzzleHttp\Client;
+
 $router = new AltoRouter();
+$client = new Client();
+$redis = new Redis();
+$redis->connect('127.0.0.1');
+
+const REDIS_STANDARD_EXPIRY = 3600;
 
 $router->map('GET', '/', function () {
     echo json_encode(['data' => 'Hello World']);
 });
 
-$router->map('GET', '/photos', function () {
-    echo json_encode(['data' => 'Get All Photos']);
+$router->map('GET', '/photos', function () use ($client, $redis) {
+    if (!$redis->exists('photos')) {
+        $response = $client->request('GET', 'https://jsonplaceholder.typicode.com/photos');
+
+        $redis->setex(
+            'photos',
+            REDIS_STANDARD_EXPIRY,
+            $response->getBody()->getContents()
+        );
+    }
+
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode([
+        'data' => json_decode($redis->get('photos'))
+    ]);
 });
 
-$router->map('GET', '/photos/[i:id]', function (int $id) {
-    echo json_encode(['data' => 'Get Photo ' . $id]);
+$router->map('GET', '/photos/[i:id]', function (int $id) use ($client, $redis) {
+    if (!$redis->exists('photos:' . $id)) {
+        $response = $client->request('GET', 'https://jsonplaceholder.typicode.com/photos/' . $id);
+
+        $redis->setex(
+            'photos:' . $id,
+            REDIS_STANDARD_EXPIRY,
+            $response->getBody()->getContents()
+        );
+    }
+
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode([
+        'data' => json_decode($redis->get('photos:' . $id))
+    ]);
 });
 
-// Todo: fix this
-//$router->map('GET', '/', 'App\Src\Controllers\PhotosController#index');
 $match = $router->match();
 
 if( is_array($match) && is_callable( $match['target'] ) ) {
@@ -26,8 +57,3 @@ if( is_array($match) && is_callable( $match['target'] ) ) {
     // no route was matched
     header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
 }
-
-//$redis = new Redis();
-//$redis->connect('127.0.0.1', 6379);
-//
-//echo $redis->ping();
